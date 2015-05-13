1c3e01e45a737697978b0e0f272a03d78886df56
sandywalker <sanddywalker@gmail.com>
2015-05-13 23:22:44 +0800

    add form input hints demo in advance

--- webui-popover/dist/jquery.webui-popover.js
+++ webui-popover/dist/jquery.webui-popover.js
@@ -53,8 +53,10 @@
 		// The actual plugin constructor
 		function WebuiPopover ( element, options ) {
 				this.$element = $(element);
-                if($.type(options.delay) === 'string' || $.type(options.delay) === 'number') {
-                    options.delay = {show:options.delay,hide:options.delay}; // bc break fix
+                if (options){
+	                if($.type(options.delay) === 'string' || $.type(options.delay) === 'number') {
+	                    options.delay = {show:options.delay,hide:options.delay}; // bc break fix
+	                }
                 }
 				this.options = $.extend( {}, defaults, options );
 				this._defaults = defaults;
@@ -69,7 +71,7 @@
 					//init the event handlers
 					if (this.getTrigger()==='click'){
 						this.$element.off('click').on('click',$.proxy(this.toggle,this));
-					}else{
+					}else if (this.getTrigger()==='hover'){
 						this.$element.off('mouseenter mouseleave click')
 										.on('mouseenter',$.proxy(this.mouseenterHandler,this))
 										.on('mouseleave',$.proxy(this.mouseleaveHandler,this))
@@ -86,7 +88,7 @@
 					this.$element.data('plugin_'+pluginName,null);
 					if (this.getTrigger()==='click'){
 						this.$element.off('click');
-					}else{
+					}else if (this.getTrigger()==='hover'){
 						this.$element.off('mouseenter mouseleave');
 					}
 					if (this.$target){
@@ -107,6 +109,7 @@
 					this.$element.trigger(e);
 					if (this.$target){this.$target.removeClass('in').hide();}
 					this.$element.trigger('hidden.'+pluginType);
+
 				},
 				toggle:function(e){
 					if (e) {
@@ -120,6 +123,7 @@
 				},
 				/*core method ,show popover */
 				show:function(){
+
 					var
 						$target = this.getTarget().removeClass().addClass(pluginClass);
 					if (!this.options.multi){
@@ -169,7 +173,7 @@
 					if (!this.options.arrow){
 						$target.find('.arrow').remove();
 					}
-					$target.remove().css({ top: -1000, left: -1000, display: 'block' });
+					$target.remove().css({ top: -2000, left: -2000, display: 'block' });
 					if (this.getAnimation()){
 						$target.addClass(this.getAnimation());
 					}
@@ -206,6 +210,7 @@
 					}
 					this._poped = true;
 					this.$element.trigger('shown.'+pluginType);
+
 
 				},
 
@@ -257,14 +262,14 @@
                     if (typeof(dataAttr) !== 'undefined') {
                         return dataAttr;
                     }
-					return this.options.delay.show||100;
+					return this.options.delay.show===0?0:this.options.delay.show||100;
 				},
                 getHideDelay:function(){
                     var dataAttr = this.$element.attr('data-delay-hide');
                     if (typeof(dataAttr) !== 'undefined') {
                         return dataAttr;
                     }
-					return this.options.delay.hide||100;
+					return this.options.delay.hide===0?0:this.options.delay.hide||100;
 				},
 				getConstrains:function(){
                     var dataAttr = this.$element.attr('data-contrains');
@@ -369,10 +374,12 @@
 					}
 				},
 				bodyClickHandler:function(){
-					if (this._targetclick){
-						this._targetclick = false;
-					}else{
-						this.hideAll();
+					if (this.getTrigger()==='click'){
+						if (this._targetclick){
+							this._targetclick = false;
+						}else{
+							this.hideAll();
+						}
 					}
 				},
 
@@ -382,7 +389,7 @@
 
 				//reset and init the target events;
 				initTargetEvents:function(){
-					if (this.getTrigger()!=='click'){
+					if (this.getTrigger()==='hover'){
 						this.$target.off('mouseenter mouseleave')
 									.on('mouseenter',$.proxy(this.mouseenterHandler,this))
 									.on('mouseleave',$.proxy(this.mouseleaveHandler,this));
